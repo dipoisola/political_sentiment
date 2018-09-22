3189b8c4d5fbed0bbb45a3192dff37772cfcf056
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-09-22 18:16:19 +0200

    [#2] Allow Circle CI to update lambda function code

--- creeper/terraform/iam.tf
+++ creeper/terraform/iam.tf
@@ -5,13 +5,40 @@ data "aws_iam_policy_document" "creeper-policy" {
     actions = ["sts:AssumeRole"]
 
     principals = {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
   }
 }
 
 resource "aws_iam_role" "creeper-iam-role" {
-    name = "creeper-function-role"
-    assume_role_policy = "${data.aws_iam_policy_document.creeper-policy.json}"
+  name               = "creeper-function-role"
+  assume_role_policy = "${data.aws_iam_policy_document.creeper-policy.json}"
+}
+
+data "aws_iam_policy_document" "circle-ci-policy-doc" {
+  statement {
+    actions = [
+      "lambda:UpdateFunctionCode",
+    ]
+
+    resources = [
+      "${aws_lambda_function.creeper-function.arn}",
+    ]
+  }
+}
+
+variable "circle-ci" {
+  default = "circle-ci"
+}
+
+resource "aws_iam_policy" "circle-ci-policy" {
+  name        = "Circle-CI-policy"
+  description = "Circle CI updates Lambda source code"
+  policy      = "${data.aws_iam_policy_document.circle-ci-policy-doc.json}"
+}
+
+resource "aws_iam_user_policy_attachment" "circle-ci-policy-attachment" {
+  user       = "${var.circle-ci}"
+  policy_arn = "${aws_iam_policy.circle-ci-policy.arn}"
 }
